d87b6bdf4258f7cd375aab48ecbfdc605435baf5
ljubicanstvena16 <joksiiib@gmail.com>
2023-09-17 18:02:03 +0200

    Implementacija funkcionalnosti na glavnoj strani

--- load_data.php
+++ load_data.php
@@ -1,24 +1,113 @@
 <?php
-    require_once 'connection.php';
+include('connection.php');
 
-    $query = "SELECT * FROM todo";
-    $result = mysqli_query($conn, $query);
+if (isset($_POST['key'])) {
 
-    // Fetch and display data
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            echo "<td>" . $row['nazivTaska'] . "</td>";
-            echo "<td>" . $row['datumPocetka'] . "</td>";
-            echo "<td>" . $row['datumZavrsetka'] . "</td>";
-            echo "<td>" . $row['osobeTaska'] . "</td>";
-            echo "<td>" . $row['opisTaska'] . "</td>";
-            echo "</tr>";
+    if ($_POST['key'] == 'insert') {
+
+        $todo_id = $_POST['todo_id'];
+
+        $nazivTaska = $_POST['nazivTaska'];
+        $datumPocetka = $_POST['datumPocetka'];
+        $datumZavrsetka = $_POST['datumZavrsetka'];
+        $osobeTaska = $_POST['osobeTaska'];
+        $opisTaska = $_POST['opisTaska'];
+
+        $check = mysqli_query($conn, "SELECT * FROM todo WHERE todo_id = '$todo_id'");
+
+        if (mysqli_num_rows($check) > 0) {
+            echo "Isti task " . $naziv . ", već postoji!";
+        } else {
+
+            $sql = "INSERT INTO `todo` (`todo_id`,`nazivTaska`, `datumPocetka`, `datumZavrsetka`,`osobeTaska`,`opisTaska`) VALUES ('$todo_id','$nazivTaska', '$datumPocetka', '$datumZavrsetka', '$osobeTaska', '$opisTaska')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Ubacen novi task!";
+            } else {
+                echo "Takav task vec postoji!";
+            }
         }
-    } else {
-        echo "<tr><td colspan='5'>No tasks found</td></tr>";
     }
 
-    // Close database connection
-    mysqli_close($conn);
+
+    if ($_POST['key'] == 'load') {
+
+        $start = $conn->real_escape_string($_POST['start']);
+        $limit = $conn->real_escape_string($_POST['limit']);
+        $sort = $conn->real_escape_string($_POST['sort']);
+        $sql = $conn->query("SELECT todo_id, nazivTaska, datumPocetka, datumZavrsetka, osobeTaska, opisTaska FROM todo ORDER BY $sort LIMIT $start, $limit");
+
+        if ($sql->num_rows > 0) {
+            $response = "";
+            while ($data = $sql->fetch_array()) {
+
+                $response .= '
+                                <tr>
+                                    <td id="todo_' . $data["todo_id"] . '">' . $data["todo_id"] . '</td>
+                                    <td>' . $data["nazivTaska"] . '</td>
+                                    <td>' . $data["datumPocetka"] . '</td>
+                                    <td>' . $data["datumZavrsetka"] . '</td>
+                                    <td>' . $data["osobeTaska"] . '</td>
+                                    <td>' . $data["opisTaska"] . '</td>
+                                    <td>
+                                        <input type="button" onclick="izmeniPogledaj(' . $data["todo_id"] . ', \'izmeni\')" value="IZMENA" class="btn btn-primary">
+                                        <input type="button" onclick="izmeniPogledaj(' . $data["todo_id"] . ', \'pogledaj\')" value="DETALJNIJI PREGLED" class="btn btn-warning">
+                                        <input type="button" onclick="izbrisi(' . $data["todo_id"] . ')" value="BRISANJE" class="btn btn-danger">
+                                    </td>
+                                </tr>
+                            ';
+            }
+            exit($response);
+        } else
+            exit('reachedMax');
+    }
+
+
+
+    if ($_POST['key'] == 'delete') {
+        $todo_id = $conn->real_escape_string($_POST['todo_id']);
+        $conn->query("DELETE FROM todo WHERE todo_id='$todo_id'");
+        exit('Task je obrisan!');
+    }
+
+    if ($_POST['key'] == 'getData') {
+        $todo_id = $conn->real_escape_string($_POST['todo_id']);
+        $sql = $conn->query("SELECT todo_id, nazivTaska, datumPocetka, datumZavrsetka, osobeTaska, opisTaska FROM todo WHERE todo_id = $todo_id");
+        $data = $sql->fetch_array();
+        $jsonArray = array(
+            'todo_id' => $data['todo_id'],
+            'nazivTaska' => $data['nazivTaska'],
+            'datumPocetka' => $data['datumPocetka'],
+            'datumZavrsetka' => $data['datumZavrsetka'],
+            'osobeTaska' => $data['osobeTaska'],
+            'opisTaska' => $data['opisTaska']
+        );
+        exit(json_encode($jsonArray));
+    }
+
+
+    if ($_POST['key'] == 'update') {
+
+        $trenutni_red = $conn->real_escape_string($_POST['todo_id']);
+
+        $nazivTaska = $_POST['nazivTaska'];
+        $datumPocetka = $_POST['datumPocetka'];
+        $datumZavrsetka = $_POST['datumZavrsetka'];
+        $osobeTaska = $_POST['osobeTaska'];
+        $opisTaska = $_POST['opisTaska'];
+
+
+
+        $sql = "UPDATE todo SET nazivTaska='$nazivTaska', datumPocetka='$datumPocetka', datumZavrsetka='$datumZavrsetka', osobeTaska='$osobeTaska',opisTaska='$opisTaska' WHERE todo_id='$trenutni_red'";
+        if ($conn->query($sql) === TRUE) {
+            echo "Uspešno izmenjen task!";
+        } else {
+            echo "Task nije izmenjen!";
+        }
+
+    }
+}
+
+mysqli_close($conn);
+
 ?>
--- register.php
+++ register.php
@@ -1,30 +1,30 @@
 <?php
-    require_once 'connection.php';
+require_once 'connection.php';
 
-    if (isset($_POST['key'])) {
+if (isset($_POST['key'])) {
 
-        $key = $_POST['key'];
+    $key = $_POST['key'];
 
-        if ($key === 'reg') {
-            $name = $_POST['name'];
-            $username = $_POST['username'];
-            $password = $_POST['password'];
+    if ($key === 'reg') {
+        $name = $_POST['name'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-            $query = mysqli_query($conn, "SELECT * FROM `users` WHERE username = '$username' AND password = '$password'");
-            
-            if (mysqli_num_rows($query) === 0) {
-                $sql = "INSERT INTO `users` (`ime`, `username`, `password`) VALUES ('$name', '$username', '$password')";
-		
-                if ($conn->query($sql) === TRUE) {
-                    echo "OK";
-                } else {
-                    echo "unsuccesful";
-                }
+        $query = mysqli_query($conn, "SELECT * FROM `users` WHERE username = '$username' AND password = '$password'");
+
+        if (mysqli_num_rows($query) === 0) {
+            $sql = "INSERT INTO `users` (`ime`, `username`, `password`) VALUES ('$name', '$username', '$password')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "OK";
             } else {
-                echo 'user already exist';
+                echo "unsuccesful";
             }
+        } else {
+            echo 'user already exist';
         }
-
-        mysqli_close($conn);
     }
+
+    mysqli_close($conn);
+}
 ?>
