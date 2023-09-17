63db702e11707da6ac2b0e2da40a671a1caed528
ljubicanstvena16 <joksiiib@gmail.com>
2023-09-17 17:39:35 +0200

    Odjavljivanje sa sistema

--- pocetna.php
+++ pocetna.php
@@ -4,20 +4,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/css/foundation.min.css" crossorigin="anonymous">
+
     <title>Todo list</title>
 </head>
 <body>
-    <h1>Todo List</h1>
+<div class="grid-container">
+        <h1>Todo List</h1>
 
-    <table>
-        <tr>
-            <th>Task Name</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-            <th>Assigned People</th>
-            <th>Description</th>
-        </tr>
-        <?php include 'load_data.php'; ?>
-    </table>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Task Name</th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
+                    <th>Assigned People</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php include 'load_data.php'; ?>
+            </tbody>
+        </table>
+
+        <button class="button alert" id="logoutButton">Odjavi se :( </button>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/js/foundation.min.js" crossorigin="anonymous"></script>
+    <script>
+
+        function logout() {
+            window.location.href = "index.html";
+        }
+
+        // Attach event listener to the logout button
+        $("#logoutButton").on("click", logout);
+    
+    </script>
 </body>
 </html>
